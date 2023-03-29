42384cc71a1217e6bbb096377c867ede2d706f2b
Shal <shal.work@mail.ru>
2023-03-29 14:53:52 +0300

    5-d commit, add vh

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 'use strict';
 
 window.addEventListener('DOMContentLoaded', function() { 
-    let width = document.querySelector('.sizes-width');
-    let height = document.querySelector('.sizes-height');
+    let box = document.querySelector('.box');
+    let width = box.querySelector('.sizes-width');
+    let height = box.querySelector('.sizes-height');
 
     width.innerHTML = document.documentElement.clientWidth + 'px'; 
     height.innerHTML = document.documentElement.clientHeight + 'px'; 
 
 
-    let offsetWidth = document.querySelector('.offset-width');
-    let offsetHeight = document.querySelector('.offset-height');
+    let offsetWidth = box.querySelector('.offset-width');
+    let offsetHeight = box.querySelector('.offset-height');
 
-    offsetWidth.innerHTML = window.screen.availWidth + 'px'; 
-    offsetHeight.innerHTML = window.screen.availHeight + 'px'; 
+    let wBox = box.getBoundingClientRect();
+
+    offsetWidth.innerHTML = wBox.width + 'px'; 
+    offsetHeight.innerHTML = wBox.height + 'px'; 
 
 });
 
