81909becf715e2b3bfcabeae52fb89637e99dc6a
Shal <shal.work@mail.ru>
2023-03-29 14:22:09 +0300

    3-d commit, add availHeight

--- script.js
+++ script.js
@@ -7,6 +7,13 @@ window.addEventListener('DOMContentLoaded', function() {
     width.innerHTML = document.documentElement.clientWidth + 'px'; 
     height.innerHTML = document.documentElement.clientHeight + 'px'; 
 
+
+    let offsetWidth = document.querySelector('.offset-width');
+    let offsetHeight = document.querySelector('.offset-height');
+
+    offsetWidth.innerHTML = window.screen.availWidth + 'px'; 
+    offsetHeight.innerHTML = window.screen.availHeight + 'px'; 
+
 });
 
 
