2f730ecab226f64abf8e74f02c4c954607bb6119
Shal <shal.work@mail.ru>
2023-04-11 13:28:00 +0300

    8-th commit, add screen

--- script.js
+++ script.js
@@ -4,9 +4,18 @@ window.addEventListener('DOMContentLoaded', function() {
     let box = document.querySelector('.box');
     let width = document.querySelector('.sizes-width');
     let height = document.querySelector('.sizes-height');
+    let screenWidth = document.querySelector('.screen-width');
+    let screenHeight = document.querySelector('.screen-height');
 
+    
+        
+    screenWidth.innerHTML = screen.width + 'px'; 
+    screenHeight.innerHTML = screen.height + 'px'; 
+    
+    
     width.innerHTML = document.documentElement.clientWidth + 'px'; 
-    height.innerHTML = document.documentElement.clientHeight + 'px'; 
+    height.innerHTML = document.documentElement.clientHeight + '-1-px'; 
+
 
     let sizes = document.querySelector('.sizes');
     let offsetWidth = sizes.querySelector('.offset-width');
@@ -16,7 +25,7 @@ window.addEventListener('DOMContentLoaded', function() {
     console.log(sizes.getBoundingClientRect());
 
     offsetWidth.innerHTML = rectSizes.width + 'px'; 
-    offsetHeight.innerHTML = rectSizes.height + 'px'; 
+    offsetHeight.innerHTML = rectSizes.height + 'px';  
 
 });
 
